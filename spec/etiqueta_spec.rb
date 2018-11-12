973dd7a7d6dfe50e14353e50a784b5fa4920a1b1
dragooncete <dragooncete@gmail.com>
2018-11-12 17:15:34 +0000

    spec to_s

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -10,7 +10,12 @@ RSpec.describe Etiqueta do
     expect(false).to eq(false)
   end
   
-  it "guarda los atributos" do
+  it "guarda el nombre" do
     expect((Etiq.new("Hamburguesa")).name).to eq("Hamburguesa")
   end
+  
+  it "guarda los atributos" do
+    expect((Etiq.new("Queso",0.1,0.09,3.5,3.5,8,0.1)).to_s).to eq("Queso, grasa 0.1g de las cuales saturada 0.09g, hidratos de carbono 3.5g de los cuales azucares 3.5g, proteinas 8g, sal 0.1g")
+  end
+  
 end
