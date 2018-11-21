6ea987aeae7a998dd6435672f640def2946a1743
dragooncete <dragooncete@gmail.com>
2018-11-21 01:36:19 +0000

    prueba calculo imc

--- spec/antropometria_spec.rb
+++ spec/antropometria_spec.rb
@@ -2,8 +2,14 @@ require "spec_helper"
 
 RSpec.describe Etiqueta do
     
+    before :each do
+        @datos = Antropometria.new(70.0,1.80,25,1,94.0,108.0)
+    end
+    
     it "crea objeto Antropometría" do
         expect((Antropometria.new(70.0,1.80,25,1,94.0,108.0)).class).to eq(Antropometria)
     end
+    
+    expect(@datos.imc).to eq(23.4)
     
 end
