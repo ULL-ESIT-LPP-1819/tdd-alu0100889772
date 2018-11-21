915402b027e55355bc9253ae54db60afb5b75bdb
dragooncete <dragooncete@gmail.com>
2018-11-21 01:41:55 +0000

    prueba %grasa

--- spec/antropometria_spec.rb
+++ spec/antropometria_spec.rb
@@ -9,8 +9,13 @@ RSpec.describe Etiqueta do
     it "crea objeto Antropometría" do
         expect((Antropometria.new(70.0,1.80,25,1,94.0,108.0)).class).to eq(Antropometria)
     end
+    
     it "calcula el imc" do
         expect(@datos.imc).to eq(23.4)
+    end
+    
+    it "calcula el porcentaje de grasa" do
+        expect(@mujer1.grasa).to eq(29.58)
     end
     
 end
